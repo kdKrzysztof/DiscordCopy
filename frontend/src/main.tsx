c9a75cc25ef38fe061694308ea72e006f6400a32
Krzysiek <57269917+kdKrzysztof@users.noreply.github.com>
2023-09-15 13:44:37 +0200

    feat: add login route

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './i18n.ts';
 import './index.css';
@@ -11,7 +11,9 @@ const client = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={client}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>
 );
