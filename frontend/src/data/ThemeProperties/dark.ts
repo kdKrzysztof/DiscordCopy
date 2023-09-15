76780c09dd1699245296a4c79ca7480054eae187
Krzysiek <57269917+kdKrzysztof@users.noreply.github.com>
2023-09-15 21:14:15 +0200

    feat: add theme properties as separate file

---
+++ frontend/src/data/ThemeProperties/dark.ts
@@ -0,0 +1,7 @@
+import type { PaletteOptions } from '@mui/material';
+
+const dark: PaletteOptions | undefined = {
+  LoginInputBG: 'rgba(0, 0, 0, 0.5)'
+} as PaletteOptions;
+
+export default dark;
---
+++ frontend/src/data/ThemeProperties/light.ts
@@ -0,0 +1,7 @@
+import type { PaletteOptions } from '@mui/material';
+
+const light: PaletteOptions | undefined = {
+  LoginInputBG: 'rgba(0, 0, 0, 0.1)'
+} as PaletteOptions;
+
+export default light;
