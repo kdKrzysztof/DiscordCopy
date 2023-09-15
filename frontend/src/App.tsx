f67c402fcea20bee4460c52f030beca80772b475
Krzysiek <57269917+kdKrzysztof@users.noreply.github.com>
2023-09-15 21:12:52 +0200

    feat: add ThemeProvider, theme switch button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,44 @@
-import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@emotion/react';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import { CssBaseline, IconButton, Tooltip } from '@mui/material';
 import { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import { Login } from 'pages';
 
+import { ThemeModeContext } from 'context';
+
+import { useThemeColor } from 'hooks';
+
 import './App.css';
 
 function App() {
+  const { theme, themeColor } = useThemeColor();
   return (
-    <>
-      <CssBaseline />
-      <Routes>
-        <Route
-          path="/login"
-          element={
-            <Suspense fallback={<></>}>
-              <Login />
-            </Suspense>
-          }
-        />
-      </Routes>
-    </>
+    <ThemeModeContext.Provider value={themeColor}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Tooltip title="Change theme">
+          <IconButton
+            onClick={themeColor.toggleColorMode}
+            color="inherit"
+            sx={{ position: 'absolute', top: '1%', right: '1%' }}>
+            {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+          </IconButton>
+        </Tooltip>
+        <Routes>
+          <Route
+            path="/login"
+            element={
+              <Suspense fallback={<></>}>
+                <Login />
+              </Suspense>
+            }
+          />
+        </Routes>
+      </ThemeProvider>
+    </ThemeModeContext.Provider>
   );
 }
 
