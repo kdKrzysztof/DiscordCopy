4f9f22fcab6177e6a4023f1400271206d4356776
Krzysiek <57269917+kdKrzysztof@users.noreply.github.com>
2023-09-15 21:17:16 +0200

    refactor: change CustomTextFieldBody background

--- frontend/src/components/LoginInput/LoginInput.styles.tsx
+++ frontend/src/components/LoginInput/LoginInput.styles.tsx
@@ -1,12 +1,14 @@
 import { Box, InputBase, styled } from '@mui/material';
 
+import { CustomPaletteOptions } from 'interfaces';
+
 export const CustomTextField = styled(InputBase)({
   marginLeft: 3,
   marginRight: 3
 });
 
-export const CustomTextFieldBody = styled(Box)({
-  background: 'rgb(0,0,0,.1)',
+export const CustomTextFieldBody = styled(Box)(({ theme }) => ({
+  background: (theme.palette as CustomPaletteOptions).LoginInputBG,
   padding: 4,
   borderRadius: 4
-});
+}));
