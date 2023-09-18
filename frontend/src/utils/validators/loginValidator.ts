b2f17cab36f91ced68d4f653ce1e2992bf648a7f
Krzysiek <57269917+kdKrzysztof@users.noreply.github.com>
2023-09-18 20:26:52 +0200

    fix: add missing validate when property is empty

--- frontend/src/utils/validators/loginValidator.ts
+++ frontend/src/utils/validators/loginValidator.ts
@@ -2,7 +2,9 @@ import * as z from 'zod';
 
 const FormSchema = z.object({
   email: z.string().email({ message: 'Email or password is not valid' }),
-  password: z.string()
+  password: z.string().refine((value) => value.trim() !== '', {
+    message: 'Input cannot be an empty string'
+  })
 });
 
 export default FormSchema;
