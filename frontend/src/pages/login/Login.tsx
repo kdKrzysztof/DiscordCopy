9cadf45fe99092c8146023f2e477d938d8af3ebd
Krzysiek <57269917+kdKrzysztof@users.noreply.github.com>
2023-09-18 13:56:26 +0200

    feat: functional login form without validation

--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -1,10 +1,10 @@
-import { Paper, Typography } from '@mui/material';
+import { Button, Link, Typography } from '@mui/material';
 import { useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 
 import { LoginInput } from 'components';
 
-import { LoginBody } from './Login.styles';
+import { LoginBody, LoginContainer, LoginTitleContainer } from './Login.styles';
 import { LoginSubmit } from './Login.types';
 
 const Login = () => {
@@ -15,14 +15,26 @@ const Login = () => {
     console.log(data);
   };
   return (
-    <Paper elevation={4}>
-      <LoginBody>
-        <Typography variant="h5">{t('LoginTitle')}</Typography>
-        <form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
-          <LoginInput />
-        </form>
-      </LoginBody>
-    </Paper>
+    <LoginContainer elevation={4}>
+      <form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
+        <LoginBody>
+          <LoginTitleContainer>
+            <Typography variant="h5" fontWeight="bold">
+              {t('LoginTitle')}
+            </Typography>
+            <Typography variant="subtitle2">{t('LoginSubtitle')}</Typography>
+          </LoginTitleContainer>
+          <LoginInput placeholder="Username" register={register} name="username" required={true} />
+          <LoginInput placeholder="Password" register={register} name="password" required={false} />
+          <Button variant="contained" fullWidth type="submit">
+            {t('LoginButton')}
+          </Button>
+          <Typography variant="subtitle2">
+            {t('LoginNeedAccount')} <Link>{t('LoginClickHere')}</Link>
+          </Typography>
+        </LoginBody>
+      </form>
+    </LoginContainer>
   );
 };
 
