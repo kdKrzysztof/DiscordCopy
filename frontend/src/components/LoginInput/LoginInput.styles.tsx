b783492c7e0e7ccae696ff281381394ac903cb2f
Krzysiek <57269917+kdKrzysztof@users.noreply.github.com>
2023-09-18 18:56:32 +0200

    refactor: change LoginTextField style

--- frontend/src/components/LoginInput/LoginInput.styles.tsx
+++ frontend/src/components/LoginInput/LoginInput.styles.tsx
@@ -1,15 +1,16 @@
 import { Box, InputBase, styled } from '@mui/material';
 
-import { CustomPaletteOptions } from 'interfaces';
+import type { CustomPaletteOptions } from 'interfaces';
 
-export const LoginTextField = styled(InputBase)({
-  marginLeft: 3,
-  marginRight: 3
-});
-
-export const LoginTextFieldBody = styled(Box)(({ theme }) => ({
-  background: (theme.palette as CustomPaletteOptions).LoginInputBG,
+export const LoginTextField = styled(InputBase)(({ theme }) => ({
   padding: 4,
   borderRadius: 4,
-  width: '100%'
+  paddingLeft: 10,
+  paddingRight: 10,
+  background: (theme.palette as CustomPaletteOptions).LoginInputBG
 }));
+
+export const LoginTextFieldBody = styled(Box)({
+  width: '100%',
+  textAlign: 'left'
+});
