a512982af37e5fbdc944b309bf6b293a3d780b9d
Krzysiek <57269917+kdKrzysztof@users.noreply.github.com>
2023-09-20 22:54:39 +0200

    feat: add stack prop to list menuItems

--- frontend/src/components/StyledSelect/StyledSelect.tsx
+++ frontend/src/components/StyledSelect/StyledSelect.tsx
@@ -1,12 +1,32 @@
-import { SelectBody } from './StyledSelect.styles';
+import { MenuItem } from '@mui/material';
+
+import { SelectBody, SelectorName } from './StyledSelect.styles';
+import SelectorMenuProps from './data/SelectorMenuProps';
 
 interface IStyledSelect {
   variant?: 'standard' | 'outlined' | 'filled' | undefined;
   placeholder?: string | undefined;
+  stack?: unknown[] | undefined;
 }
 
-const StyledSelect = ({ variant, placeholder }: IStyledSelect) => {
-  return <SelectBody variant={variant} placeholder={placeholder} />;
+const StyledSelect = ({ variant, placeholder, stack }: IStyledSelect) => {
+  return (
+    <SelectBody
+      variant={variant}
+      value={placeholder}
+      label={placeholder}
+      MenuProps={SelectorMenuProps}>
+      <MenuItem disabled value={placeholder}>
+        <SelectorName>{placeholder}</SelectorName>
+      </MenuItem>
+      {stack &&
+        stack.map((el) => (
+          <MenuItem key={String(el)} value={String(el)}>
+            {String(el)}
+          </MenuItem>
+        ))}
+    </SelectBody>
+  );
 };
 
 export default StyledSelect;
