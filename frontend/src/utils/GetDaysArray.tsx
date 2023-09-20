4b1cd334c6758b24dbb7432dafa743fcd5a372cd
Krzysiek <57269917+kdKrzysztof@users.noreply.github.com>
2023-09-20 22:24:03 +0200

    feat: add GetDaysArray function

---
+++ frontend/src/utils/GetDaysArray.tsx
@@ -0,0 +1,12 @@
+/**
+ * @param to This parameter is required. Target value up to which the array should be generated.
+ * @param from This parameter is optional. If provided, it specifies the starting value for the array. The function will start generating the array from this value. If "from" is not provided, the function will default to starting from 1.
+ */
+
+const useGetNumberArray = (to: number, from?: number) => {
+  return Array.from({ length: from ? to - from + 1 : to }, (_, index) =>
+    from ? from + index : index + 1
+  );
+};
+
+export default useGetNumberArray;
