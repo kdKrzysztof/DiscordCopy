0bed360fdc23bb1eb958554b66ac22584e818ff7
Krzysiek <57269917+kdKrzysztof@users.noreply.github.com>
2023-09-20 17:09:28 +0200

    chore: add absolute import for new components

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,2 +1,4 @@
 export { default as StyledTextfield } from './StyledTextfield';
 export { default as ChangeThemeButton } from './ChangeThemeButton';
+export { default as StyledSelect } from './StyledSelect';
+export { default as RegisterDatepicker } from './RegisterDatepicker';
