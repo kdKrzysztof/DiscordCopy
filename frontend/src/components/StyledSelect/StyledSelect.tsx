4d7de95464dbdb68f0a741f358f97ab039ba1f94
Krzysiek <57269917+kdKrzysztof@users.noreply.github.com>
2023-09-21 17:15:49 +0200

    feat: add form register hook

--- frontend/src/components/StyledSelect/StyledSelect.tsx
+++ frontend/src/components/StyledSelect/StyledSelect.tsx
@@ -1,27 +1,46 @@
-import { MenuItem } from '@mui/material';
+import { MenuItem, SelectChangeEvent } from '@mui/material';
+import { useState } from 'react';
+import type { FieldValues, Path, UseFormRegister } from 'react-hook-form';
 
 import { SelectBody, SelectorName } from './StyledSelect.styles';
 import SelectorMenuProps from './data/SelectorMenuProps';
 
-interface IStyledSelect {
+interface IStyledSelect<TForm extends FieldValues> {
   variant?: 'standard' | 'outlined' | 'filled' | undefined;
   placeholder?: string | undefined;
   stack?: unknown[] | undefined;
+  register: UseFormRegister<TForm>;
+  formFieldName: Path<TForm>;
 }
 
-const StyledSelect = ({ variant, placeholder, stack }: IStyledSelect) => {
+const StyledSelect = <TForm extends FieldValues>({
+  variant,
+  placeholder,
+  stack,
+  register,
+  formFieldName
+}: IStyledSelect<TForm>) => {
+  const [value, setValue] = useState<string | number | undefined>('');
+
+  const handleChange = (event: SelectChangeEvent<unknown>) => {
+    setValue(parseInt(event.target.value as string));
+  };
   return (
     <SelectBody
+      required
       variant={variant}
-      value={placeholder}
       label={placeholder}
+      displayEmpty
+      value={value}
+      {...register(formFieldName)}
+      onChange={handleChange}
       MenuProps={SelectorMenuProps}>
-      <MenuItem disabled value={placeholder}>
+      <MenuItem disabled value="">
         <SelectorName>{placeholder}</SelectorName>
       </MenuItem>
       {stack &&
-        stack.map((el) => (
-          <MenuItem key={String(el)} value={String(el)}>
+        stack.map((el: unknown) => (
+          <MenuItem key={String(el)} value={Number(el)}>
             {String(el)}
           </MenuItem>
         ))}
