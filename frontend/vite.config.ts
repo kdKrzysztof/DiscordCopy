7769d3b229c05750227b3397ec2e47565194e74f
Krzysiek <57269917+kdKrzysztof@users.noreply.github.com>
2023-09-28 15:00:22 +0200

    fix: vite config endpoint target

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
@@ -17,7 +17,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: process.env.API,
+        target: process.env.VITE_API,
         changeOrigin: true,
         secure: false,
         ws: true
