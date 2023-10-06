bfd420bed51971654d6e9d73def650146173e184
Krzysiek <57269917+kdKrzysztof@users.noreply.github.com>
2023-10-06 22:25:11 +0200

    feat: add Provider to main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,11 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+
+import { store } from 'store';
+
 import App from './App.tsx';
 import './i18n.ts';
 import './index.css';
@@ -11,9 +15,11 @@ const client = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={client}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
     </QueryClientProvider>
   </React.StrictMode>
 );
