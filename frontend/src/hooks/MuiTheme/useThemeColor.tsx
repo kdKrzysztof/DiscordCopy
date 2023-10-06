ef0a52e5157db95a42c21a4ec37c681e8a9681db
Krzysiek <57269917+kdKrzysztof@users.noreply.github.com>
2023-10-06 22:25:40 +0200

    refactor: simplify useThemeColor logic

--- frontend/src/hooks/MuiTheme/useThemeColor.tsx
+++ frontend/src/hooks/MuiTheme/useThemeColor.tsx
@@ -1,29 +1,20 @@
-import type { PaletteMode } from '@mui/material';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect } from 'react';
+
+import { useAppSelector } from 'hooks/reduxHooks';
 
 import usePageTheme from './usePageTheme';
 
 const useThemeColor = () => {
-  const [themeColorState, setThemeColorState] = useState<PaletteMode>(
-    (localStorage.getItem('themeColor') as PaletteMode) ?? 'light'
-  );
+  const themeColor = useAppSelector((state) => state.theme);
 
+  const theme = usePageTheme(themeColor);
+  
   useEffect(() => {
-    localStorage.setItem('themeColor', themeColorState);
-  }, [themeColorState]);
+    localStorage.setItem('themeColor', themeColor);
+  }, [themeColor]);
 
-  const themeColor = useMemo(
-    () => ({
-      toggleColorMode: () => {
-        setThemeColorState((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
-      }
-    }),
-    []
-  );
 
-  const theme = usePageTheme(themeColorState);
-
-  return { themeColor, theme };
+  return theme;
 };
 
 export default useThemeColor;
